a02c926ff0bf637a6261d2874799f9d0559de3c0
aditya <adityasharma5119@gmail.com>
2022-01-20 01:33:15 +0530

    config

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,51 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
-const Details = () => {
-  const { id } = useParams();
-  return <h2>{id}</h2>;
-};
+// const Details = () => {
+//   const { id } = useParams();
+//   return <h2>{id}</h2>;
+// };
 
-export default Details;
+// export default Details;
+
+
+/**Dtails.js as a class component,instead of function component */
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    console.log(this.state);
+
+    if (this.state.loading) {
+      return <h2>loading … </h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
